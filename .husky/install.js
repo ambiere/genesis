a222888e9e6a31cf706ae50090750a09f731c744
zhid0399123 <zhid0399123@gmail.com>
2024-01-25 20:37:21 +0300

    fix: wrapped husky importation in async fn

--- .husky/install.js
+++ .husky/install.js
@@ -1,5 +1,8 @@
 if (process.env.NODE_ENV === "production" || process.env.CI === "1") {
   process.exit(0)
 }
-const husky = await import("husky")
-husky()
+
+;(async () => {
+  const husky = await import("husky")
+  husky()
+})()
