0a9b58af67d3f53b3477a2f0778f8b735b152e42
zhid0399123 <zhid0399123@gmail.com>
2024-01-25 12:35:30 +0300

    feat(cli-log): genes cli log util

---
+++ src/utils/cli-log.js
@@ -0,0 +1,23 @@
+'use strict'
+const log = require('../utils/log')
+
+const logUtil = {
+  dirExist: (_dir) => {
+    log('error', `[dirError] Directory ${_dir} already exists`)
+    process.exit(1)
+  },
+  dirUndefined: () => {
+    log('error', "[dirError] Must specify a directory to 'genes'")
+    process.exit(1)
+  },
+  pkgExist: () => {
+    log('error', '[pkgError] A package.json file already exists in target directory')
+    process.exit(1)
+  },
+  warn: (prop) => {
+    const msg = prop === 'username' ? 'author name' : 'author email'
+    log('warn', `\n[WARN] ${prop} option not provided and the ${msg} in package.json is empty. <${prop}> placeholder not updated\n`)
+  }
+}
+
+module.exports = logUtil
