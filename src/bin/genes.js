d52364971e042542bfa662ed32590f75a7c82dc8
zhid0399123 <zhid0399123@gmail.com>
2024-01-26 14:35:02 +0300

    feat(genes): print out help text when running `genes` without command

--- src/bin/genes.js
+++ src/bin/genes.js
@@ -6,12 +6,13 @@ const argv = require('yargs-parser')(process.argv)
 const help = require('help-me')({ dir: path.join(path.dirname(require.main.filename), '../..', 'help') })
 
 const generate = require('../../genes')
+commist.register('', () => console.log('called'))
 commist.register('generate', generate.genesCli)
 commist.register('help', help.toStdout)
 commist.register('version', function () {
   console.log(require('../../package.json').version)
 })
 
-if (argv.help) {
+if (argv.help || (Object.keys(argv).length === 1 && argv._.splice(2).length === 0)) {
   help.toStdout(typeof argv.help === 'string' && argv.help !== 'version' ? argv.help : undefined)
 } else commist.parse(process.argv.splice(2).filter((arg) => arg !== 'version'))
