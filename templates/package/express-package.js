e556ed6602c51d363c0be86e2d2f88b6d6cb0727
zhid0399123 <zhid0399123@gmail.com>
2024-02-10 18:00:06 +0300

    feat(package): updated package info method

--- templates/package/express-package.js
+++ templates/package/express-package.js
@@ -1,68 +1,73 @@
-const color = require('kolorist')
-const log = require('../../src/utils/log.js')
-const pkg = require('../../package.json')
+const { bold, green } = require("kolorist")
+const log = require("../../src/utils/log.js")
+const pkg = require("../../package.json")
+const plus = require("../../src/utils/plus.js")
 
-const fastifyPackageTemplate = {
-  dir: 'app/express',
-  main: 'src/bin/index.js',
-  tye: 'commonjs',
+const expressPackageTemplate = {
+  dir: "app/express",
+  main: "src/bin/index.js",
+  tye: "commonjs",
   directories: {
-    test: 'test',
-    src: 'src',
-    public: 'public'
+    test: "test",
+    src: "src",
+    public: "public",
   },
   scripts: {
-    lint: 'standard',
-    'lint:fix': 'standard --fix',
-    pretest: 'pnpm run lint',
+    lint: "standard",
+    "lint:fix": "standard --fix",
+    pretest: "pnpm run lint",
     test: 'cross-env NODE_ENV=test nyc tap "test/**/*.test.js"',
-    start: 'node src/bin/index.js',
-    dev: 'nodemon --watch src/ src/bin/index.js',
+    start: "node src/bin/index.js",
+    dev: "nodemon --watch src/ src/bin/index.js",
     prepare: "node .husky/install.js",
-    version: 'cz bump',
-    'docker:compose': 'docker-compose -f docker-compose.yml up'
+    version: "cz bump",
+    "docker:compose": "docker-compose -f docker-compose.yml up",
   },
   mongoScripts: {
     test: 'cross-env NODE_ENV=test nyc tap --before=test/dockerode/run-before.js "test/**/*.test.js" --after=test/dockerode/run-after.js --no-check-coverage',
-    'test:nostop': 'cross-env NODE_ENV=test nyc tap --before=test/dockerode/run-before.js "test/**/*.test.js" --no-check-coverage',
-    'test:coverage':
+    "test:nostop": 'cross-env NODE_ENV=test nyc tap --before=test/dockerode/run-before.js "test/**/*.test.js" --no-check-coverage',
+    "test:coverage":
       'cross-env NODE_ENV=test nyc tap --coverage-report=html --before=test/dockerode/run-before.js "test/**/*.test.js" --after=test/dockerode/run-after.js --no-check-coverage',
-    dev: 'pnpm run mongo:start && nodemon --watch src/ src/bin/index.js',
-    'mongo:start': 'docker run -d -p 27017:27017 --rm',
-    'mongo:stop': 'docker container stop'
+    dev: "pnpm run mongo:start && nodemon --watch src/ src/bin/index.js",
+    "mongo:start": "docker run -d -p 27017:27017 --rm",
+    "mongo:stop": "docker container stop",
   },
-  license: 'MIT',
+  license: "MIT",
   dependencies: {
     compression: pkg.dependencies.compression,
     cors: pkg.dependencies.cors,
     dotenv: pkg.dependencies.dotenv,
     express: pkg.dependencies.express,
     kolorist: pkg.dependencies.kolorist,
-    'pino-pretty': pkg.devDependencies['pino-pretty'] || pkg.dependencies['pino-pretty'],
-    'pino-http': pkg.devDependencies['pino-http'] || pkg.dependencies['pino-http']
+    "pino-pretty": pkg.devDependencies["pino-pretty"] || pkg.dependencies["pino-pretty"],
+    "pino-http": pkg.devDependencies["pino-http"] || pkg.dependencies["pino-http"],
   },
   devDependencies: {
-    'cross-env': pkg.devDependencies['cross-env'],
+    "cross-env": pkg.devDependencies["cross-env"],
     husky: pkg.devDependencies.husky,
-    'lint-staged': pkg.devDependencies['lint-staged'],
+    "lint-staged": pkg.devDependencies["lint-staged"],
     nodemon: pkg.devDependencies.nodemon,
     nyc: pkg.devDependencies.nyc,
     supertest: pkg.devDependencies.supertest,
     standard: pkg.devDependencies.standard,
-    tap: pkg.devDependencies.tap
+    tap: pkg.devDependencies.tap,
   },
   devDependenciesOpts: {
-    dockerode: '^4.0.0'
+    dockerode: "^4.0.0",
   },
-  info: (pkg) => {
-    log('debug', '[pkg] Saved package.json')
-    log('info', `[${pkg.name}] Generated successfully`)
-    log('debug', `[${color.bold('npm install')}] Install the dependencies`)
-    log('debug', `[${color.bold('npm start')}] Start the application`)
-    log('debug', `[${color.bold('npm run dev')}] Start the application in dev`)
-    log('debug', `[${color.bold('npm test')}] Execute the unit tests`)
-    log('debug', `[${color.bold('npm lint')}] Lint and fix code style issues`)
-  }
+  info: (pkg, dest) => {
+    log("debug", "5: [pkg] saved package.json")
+    log(`debug`, `${green("+")}     package.json ${plus(`package.json`)}\n`)
+    log("info", `[${pkg.name}] Generated successfully!\n`)
+    log("info", "\x1B[2K՞༅⸙⁂〰༅꙳৽՞⸙꙳⁂〰৽\n\n Happy coding dawg!")
+
+    console.log(`\n get started, run:\n`)
+    log("debud", ` cd ${dest}`)
+    log("debug", " pnpm install")
+    log("debug", " pnpm start")
+    log("debug", " pnpm test")
+    log("debug", " pnpm lint\n")
+  },
 }
 
-module.exports = fastifyPackageTemplate
+module.exports = expressPackageTemplate
--- templates/package/fastify-package.js
+++ templates/package/fastify-package.js
@@ -1,66 +1,71 @@
-const color = require('kolorist')
-const log = require('../../src/utils/log.js')
-const pkg = require('../../package.json')
+const { bold, green } = require("kolorist")
+const log = require("../../src/utils/log.js")
+const pkg = require("../../package.json")
+const plus = require("../../src/utils/plus.js")
 
 const fastifyPackageTemplate = {
-  dir: 'app/fastify',
-  main: 'src/server.js',
-  tye: 'commonjs',
+  dir: "app/fastify",
+  main: "src/server.js",
+  tye: "commonjs",
   directories: {
-    test: 'test',
-    src: 'src'
+    test: "test",
+    src: "src",
   },
   scripts: {
-    lint: 'standard',
-    'lint:fix': 'standard --fix',
-    pretest: 'pnpm run lint',
+    lint: "standard",
+    "lint:fix": "standard --fix",
+    pretest: "pnpm run lint",
     test: 'cross-env NODE_ENV=test tap "test/**/*.test.js" --no-check-coverage',
-    'test:coverage': 'cross-env NODE_ENV=test tap --coverage-report=html test/**/*.test.js" --no-check-coverage',
-    start: 'cross-env NODE_ENV=production fastify start -l info --options src/server.js',
-    dev: 'fastify start --options -w --debug src/server.js',
+    "test:coverage": 'cross-env NODE_ENV=test tap --coverage-report=html test/**/*.test.js" --no-check-coverage',
+    start: "cross-env NODE_ENV=production fastify start -l info --options src/server.js",
+    dev: "fastify start --options -w --debug src/server.js",
     prepare: "node .husky/install.js",
-    version: 'cz bump',
-    'docker:compose': 'docker-compose -f docker-compose.yml up'
+    version: "cz bump",
+    "docker:compose": "docker-compose -f docker-compose.yml up",
   },
   mongoScripts: {
     test: 'cross-env NODE_ENV=test tap --before=test/dockerode/run-before.js "test/**/*.test.js" --after=test/dockerode/run-after.js --no-check-coverage',
-    'test:nostop': 'cross-env NODE_ENV=test tap --before=test/dockerode/run-before.js "test/**/*.test.js" --no-check-coverage',
-    'test:coverage':
+    "test:nostop": 'cross-env NODE_ENV=test tap --before=test/dockerode/run-before.js "test/**/*.test.js" --no-check-coverage',
+    "test:coverage":
       'cross-env NODE_ENV=test tap --coverage-report=html --before=test/dockerode/run-before.js "test/**/*.test.js" --after=test/dockerode/run-after.js --no-check-coverage',
-    dev: 'pnpm run mongo:start && fastify start --options -w -T 0 --debug src/server.js ',
-    'mongo:start': 'docker run -d -p 27017:27017 --rm',
-    'mongo:stop': 'docker container stop'
+    dev: "pnpm run mongo:start && fastify start --options -w -T 0 --debug src/server.js ",
+    "mongo:start": "docker run -d -p 27017:27017 --rm",
+    "mongo:stop": "docker container stop",
   },
-  license: 'MIT',
+  license: "MIT",
   dependencies: {
-    '@fastify/autoload': pkg.dependencies['@fastify/autoload'],
-    '@fastify/cors': pkg.dependencies['@fastify/cors'],
-    '@fastify/env': pkg.dependencies['@fastify/env'],
-    '@fastify/sensible': pkg.dependencies['@fastify/sensible'],
+    "@fastify/autoload": pkg.dependencies["@fastify/autoload"],
+    "@fastify/cors": pkg.dependencies["@fastify/cors"],
+    "@fastify/env": pkg.dependencies["@fastify/env"],
+    "@fastify/sensible": pkg.dependencies["@fastify/sensible"],
     fastify: pkg.dependencies.fastify,
-    'fastify-cli': pkg.dependencies['fastify-cli'],
-    'fastify-plugin': pkg.devDependencies['fastify-plugin'] || pkg.dependencies['fastify-plugin'],
-    'pino-pretty': pkg.devDependencies['pino-pretty'] || pkg.dependencies['pino-pretty']
+    "fastify-cli": pkg.dependencies["fastify-cli"],
+    "fastify-plugin": pkg.devDependencies["fastify-plugin"] || pkg.dependencies["fastify-plugin"],
+    "pino-pretty": pkg.devDependencies["pino-pretty"] || pkg.dependencies["pino-pretty"],
   },
   devDependencies: {
-    'cross-env': pkg.devDependencies['cross-env'],
+    "cross-env": pkg.devDependencies["cross-env"],
     husky: pkg.devDependencies.husky,
-    'lint-staged': pkg.devDependencies['lint-staged'],
+    "lint-staged": pkg.devDependencies["lint-staged"],
     standard: pkg.devDependencies.standard,
-    tap: pkg.devDependencies.tap
+    tap: pkg.devDependencies.tap,
   },
   devDependenciesOpts: {
-    dockerode: '^4.0.0'
+    dockerode: "^4.0.0",
   },
-  info: (pkg) => {
-    log('debug', '[pkg] Saved package.json')
-    log('info', `[${pkg.name}] Generated successfully`)
-    log('debug', `[${color.bold('npm install')}] Install the dependencies`)
-    log('debug', `[${color.bold('npm start')}] Start the application`)
-    log('debug', `[${color.bold('npm run dev')}] Start the application in dev`)
-    log('debug', `[${color.bold('npm test')}] Execute the unit tests`)
-    log('debug', `[${color.bold('npm lint')}] Lint and fix code style issues`)
-  }
+  info: (pkg, dest) => {
+    log("debug", "5: [pkg] saved package.json")
+    log(`debug`, `${green("+")}     package.json ${plus(`package.json`)}\n`)
+    log("info", `[${pkg.name}] Generated successfully!\n`)
+    log("info", "\x1B[2K՞༅⸙⁂〰༅꙳৽՞⸙꙳⁂〰৽\n\n Happy coding dawg!")
+
+    console.log(`\n get started, run:\n`)
+    log("debud", ` cd ${dest}`)
+    log("debug", " pnpm install")
+    log("debug", " pnpm start")
+    log("debug", " pnpm test")
+    log("debug", " pnpm lint\n")
+  },
 }
 
 module.exports = fastifyPackageTemplate
