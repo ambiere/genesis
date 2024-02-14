f366927c0f0999987cf7b71c3ef1faf04940e13a
zhid0399123 <zhid0399123@gmail.com>
2024-02-14 03:40:47 +0300

    eat(prompt-logger): made prompt-logger UI pretty
        Displays a structured and responsive table containing
        project information provided interactively by the user in the terminal.
    
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- src/prompt/prompt-logger.js
+++ src/prompt/prompt-logger.js
@@ -1,24 +1,33 @@
-const kolorist = require("kolorist")
+const { gray, bold } = require("kolorist")
 
 function logProjectInfo(response) {
-  console.log(
-    `
-${kolorist.gray("                                  ...\n")}
-  ${kolorist.bold(response["--name"])} with provided information below will be generated at ${kolorist.bold(response["directory"])}
-  
-                             ${kolorist.gray("name")}: ${kolorist.bold(response["--name"])}
-                         ${kolorist.gray("username")}: ${kolorist.bold(response["--username"])}
-                           ${kolorist.gray("author")}: ${kolorist.bold(response["--author"])}
-                        ${kolorist.gray("framework")}: ${kolorist.bold(response["--template"])}
-                        ${kolorist.gray("directory")}: ${kolorist.bold(response["directory"])}
-                      ${kolorist.gray("description")}: ${kolorist.bold(response["--description"])}
-                          ${kolorist.gray("mongodb")}: ${kolorist.bold(response["--mongo"])}
-                            ${kolorist.gray("email")}: ${kolorist.bold(response["--email"])}
-                        ${kolorist.gray("copyright")}: ${kolorist.bold("(c) " + response["--copyright"].copyrightYear + " " + response["--copyright"].copyrightName)}
-                          ${kolorist.gray("license")}: ${kolorist.bold(response["--license"])}
-${kolorist.gray("\n                                ...\n")}
-`
-  )
+  const lenPJN = response["--name"].length - 12
+  const lenDir = response["directory"].length - 9
+  const lenAuth = response["--author"].length - 6
+  const pjnSpace = " ".repeat(lenPJN > 0 ? lenPJN : 0)
+  const dirSpace = " ".repeat(lenDir > 0 ? lenDir : 0)
+  const dividerOffset = response["--author"].length / 2
+  const authSpace = " ".repeat(lenAuth > 0 ? lenAuth : 0)
+  const pjnSpace2 = " ".repeat(lenPJN < 0 ? 12 - response["--name"].length : 0)
+  const dirSpace2 = " ".repeat(lenDir < 0 ? 9 - response["directory"].length : 0)
+  const lenDivider = `${response[`--name`]} with provided information below will be generated at ${response["directory"]}`.length + dividerOffset
+
+  // This is definitely a table and trust me dawg, it is responsive
+  // You touch anything, you'll probably spend some time on it. Good luck tho:)
+  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>TABLE
+  console.log(`                                             
+  ${bold(response["--name"])} with provided information below will be generated at ${bold(response["directory"])}\n
+  ${gray(`-`.repeat(lenDivider))}
+  ${gray(`project_name`)}${pjnSpace}  |  ${gray(`framework`)}   |  ${gray(`directory`)}${dirSpace}  |  ${gray(`author`)}${authSpace}  |  ${gray(`mongodb`)}
+  ${gray(`-`.repeat(lenDivider))}
+  ${response[`--name`]}${pjnSpace2}  |  ${response[`--template`]}     |  ${response[`directory`]}${dirSpace2}  |  ${response[`--author`]}  |  ${response[`--mongo`]}
+  ${gray(`-`.repeat(lenDivider))}`)
+  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>TABLE
+  console.log()
+  console.log(`  ${gray("Description")}: ${response["--description"]}`)
+  console.log(`  ${gray("License")}: ${response["--license"]}`)
+  console.log(`  ${gray("Copyright")}: (c) ${response["--copyright"].copyrightYear + " " + response["--copyright"].copyrightName + " " + response["--email"]}`)
+  console.log("\n")
 }
 
 module.exports = logProjectInfo
