4e67efe56b38a5426e73824e9ff5f464c89c9a28
zhid0399123 <zhid0399123@gmail.com>
2024-02-14 20:34:43 +0300

    feat(prompt): prompt user to create gh repo
    Create public github repository if user agree.
    
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- src/prompt/prompt.js
+++ src/prompt/prompt.js
@@ -1,55 +1,73 @@
 const prompts = require("prompts")
-const kolorist = require("kolorist")
-const generate = require("../../genes")
+const { gray, bold } = require("kolorist")
+const generate = require("../../genesis")
 const questions = require("./prompt-questions")
 const pkg = require("../../package.json")
 const Spinner = require("./prompt-spinner")
 const logProjectInfo = require("../prompt/prompt-logger")
+const createRepo = require("../utils/create-repo")
 
-async function genesCliPrompts() {
+async function genesisCliPrompts() {
   const spinner = new Spinner()
-  spinner.start(`starting genes CLI v${pkg.version}...`)
+  showSpinner(spinner, `starting genesis v${pkg.version}...`)
   setTimeout(async () => {
     spinner.stop(true)
-    console.log(kolorist.bold(`genes CLI v${pkg.version}`))
-    console.log(kolorist.gray(`command line interface to generate`))
-    console.log(kolorist.gray(`Node.js web framework template projects!\n`))
+    logCliInfo(pkg)
     if (spinner.isStopped) {
       const response = await prompts(questions.projectInfoQuestions)
-      if (Object.keys(response).length === 10) {
-        console.log()
-        spinner.start(`processing ${response["--name"]} data...`)
+      const answeredAllQuestions = Object.keys(response).length === 11
+      if (answeredAllQuestions) {
+        showSpinner(spinner, `processing ${response["--name"]} data...`)
         setTimeout(async () => {
           spinner.stop(true)
           if (spinner.isStopped) {
             logProjectInfo(response)
-            const confirm = await prompts(questions.confirmationQuestion(response))
-            if (confirm.confirmed) {
-              const projectInfo = {
-                ...response,
-                "--email": response["--email"] === "team@domain.com" ? "" : response["--email"],
-                "--copyrightName": response["--copyright"].copyrightName,
-                "--copyrightYear": response["--copyright"].copyrightYear,
-              }
-              delete projectInfo["--copyright"]
-              spinner.start(`generating ${response["--name"]} at ${response["directory"]}`)
+            const confirmProjectGenerate = await prompts(questions.confirmationQuestion(response))
+            if (confirmProjectGenerate.confirmed) {
+              const projectInfo = processProjectInfo(response)
+              showSpinner(spinner, `generating ${response["--name"]} at ${response["directory"]}`)
               setTimeout(async () => {
                 spinner.stop(true)
                 if (spinner.isStopped) {
                   console.log()
-                  await generate.genesCli(
+                  await generate.genesisCli(
                     Object.entries(projectInfo)
                       .flat()
                       .filter((value) => value != "directory")
                   )
+                  if (response.confirmed) {
+                    await createRepo(response)
+                  }
                 }
-              }, 1500)
+              }, 2000)
             }
           }
-        }, 1000)
+        }, 1500)
       }
     }
-  }, 1000)
+  }, 1500)
 }
 
-module.exports = genesCliPrompts
+const processProjectInfo = (response) => {
+  const projectInfo = {
+    ...response,
+    "--email": response["--email"] === "team@domain.com" ? "" : response["--email"],
+    "--copyrightName": response["--copyright"].copyrightName,
+    "--copyrightYear": response["--copyright"].copyrightYear,
+  }
+  delete projectInfo["--copyright"]
+  return projectInfo
+}
+
+const showSpinner = (spinner, message) => {
+  console.log()
+  spinner.start(message)
+}
+
+const logCliInfo = (pkg) => {
+  console.log(gray(`genesis v${pkg.version}`))
+  console.log(gray(`\nServer projects template generator for nodejs frameworks.`))
+  console.log(gray(`Copyright (c) ${pkg.author}\n`))
+}
+
+module.exports = genesisCliPrompts
