a66da87d931616e791ae6df1864fed762cb20629
zhid0399123 <zhid0399123@gmail.com>
2024-02-14 20:38:12 +0300

    feat(prompt-logger): made prompt-logger UI pretty
     Displays a structured and responsive table containing
     project information provided interactively by the user in the terminal.
    
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- src/prompt/prompt-logger.js
+++ src/prompt/prompt-logger.js
@@ -6,21 +6,21 @@ function logProjectInfo(response) {
   const lenAuth = response["--author"].length - 6
   const pjnSpace = " ".repeat(lenPJN > 0 ? lenPJN : 0)
   const dirSpace = " ".repeat(lenDir > 0 ? lenDir : 0)
-  const dividerOffset = response["--author"].length / 2
+  const dividerOffset = response["--author"].length
   const authSpace = " ".repeat(lenAuth > 0 ? lenAuth : 0)
   const pjnSpace2 = " ".repeat(lenPJN < 0 ? 12 - response["--name"].length : 0)
   const dirSpace2 = " ".repeat(lenDir < 0 ? 9 - response["directory"].length : 0)
   const lenDivider = `${response[`--name`]} with provided information below will be generated at ${response["directory"]}`.length + dividerOffset
 
-  // This is definitely a table and trust me dawg, it is responsive
+  // This is definitely a table and trust me dawg, it is responsive (shit happens)
   // You touch anything, you'll probably spend some time on it. Good luck tho:)
   //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>TABLE
   console.log(`                                             
   ${bold(response["--name"])} with provided information below will be generated at ${bold(response["directory"])}\n
   ${gray(`-`.repeat(lenDivider))}
-  ${gray(`project_name`)}${pjnSpace}  |  ${gray(`framework`)}   |  ${gray(`directory`)}${dirSpace}  |  ${gray(`author`)}${authSpace}  |  ${gray(`mongodb`)}
+    ${gray(`project_name`)}${pjnSpace}  |  ${gray(`framework`)}   |  ${gray(`directory`)}${dirSpace}  |  ${gray(`author`)}${authSpace}  |  ${gray(`mongodb`)}
   ${gray(`-`.repeat(lenDivider))}
-  ${response[`--name`]}${pjnSpace2}  |  ${response[`--template`]}     |  ${response[`directory`]}${dirSpace2}  |  ${response[`--author`]}  |  ${response[`--mongo`]}
+    ${response[`--name`]}${pjnSpace2}  |  ${response[`--template`]}     |  ${response[`directory`]}${dirSpace2}  |  ${response[`--author`]}  |  ${response[`--mongo`]}
   ${gray(`-`.repeat(lenDivider))}`)
   //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>TABLE
   console.log()
